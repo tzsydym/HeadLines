2a77f5fe55543cbd07657e8bd072896f8686cc2c
tzsydym <38777125+tzsydym@users.noreply.github.com>
2018-05-01 18:12:37 +0800

    Update Question3.js

--- Question3.js
+++ Question3.js
@@ -8,9 +8,14 @@ function add(a) {
             sum = sum+ b;
             return temp;
         }
-    }  
+    }
+    //override/add toSting and valueOf method for temp
     temp.toString = temp.valueOf = function() {
         return sum; 
     }
     return temp;
 }
+//the type of add(1)(2)(3) is function but with value of 6
+//add(1)(2)(3)() will return a number with value 6
+//in some context, like document.getElementById("p1").innerHTML = add(1)(2)(3)
+//overrided toString() or valueOf() will be called and we can get the answer
