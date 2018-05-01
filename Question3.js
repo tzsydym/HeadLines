caf987b5a48bb28f9ec07546d81b9da9b8de658f
tzsydym <38777125+tzsydym@users.noreply.github.com>
2018-05-01 16:33:02 +0800

    Question3
    Question3

---
+++ Question3.js
@@ -0,0 +1,18 @@
+var curry = function(fn) {
+    var _arg = [];
+    return function temp() {
+        if (arguments.length === 0) {
+            return fn.apply(null, _arg);
+        } else {
+            _arg = _arg.concat([].slice.call(arguments));
+            return temp;
+        }
+    }
+}
+const add = curry(function() {    
+    var i=0,len = arguments.length,sum = 0;
+    for (i; i<len; i+=1) {
+        sum = createMathOperation((augend, addend) => augend + addend, 0)(sum,arguments[i])        
+    }
+    return sum
+});
